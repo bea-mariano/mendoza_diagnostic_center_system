4dadadb3a832d3491aed4c64a5fa0d05072b6dd7
bea-mariano <beauymariano.dev@gmail.com>
2025-04-22 06:14:53 +0800

    working auto check based on the package

--- mdc_app/setpackages/urls.py
+++ mdc_app/setpackages/urls.py
@@ -1,6 +1,16 @@
 # setpackages/urls.py
 from django.urls import path
 from . import views
+from .views import (
+    SetpackageListView,
+    SetpackageDetailView,
+    SetpackageCreateView,
+    SetpackageUpdateView,
+    SetpackageDeleteView,
+    get_available_transaction_purposes,
+    get_tests_by_transaction_purpose,
+)
+
 
 urlpatterns = [
     path('setpackages/list/', views.SetpackageListView.as_view(), name='setpackage_list'),
@@ -8,4 +18,6 @@ urlpatterns = [
     path('setpackages/new/', views.SetpackageCreateView.as_view(), name='setpackage_new'),
     path('setpackages/<int:pk>/edit/', views.SetpackageUpdateView.as_view(), name='setpackage_edit'),
     path('setpackages/<int:pk>/delete/', views.SetpackageDeleteView.as_view(), name='setpackage_delete'),
+    path('ajax/get-available-transaction-purposes/', get_available_transaction_purposes, name='get_available_transaction_purposes'),
+    path('ajax/get-tests-by-purpose/<str:purpose>/', get_tests_by_transaction_purpose, name='get_tests_by_transaction_purpose'),
 ]
--- mdc_app/setpackages/views.py
+++ mdc_app/setpackages/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.http import JsonResponse
 from .models import Setpackage, SetpackageTest
 from .forms import SetpackageForm
 from tests.models import Test
+from django.views.decorators.http import require_GET
 
 
 @method_decorator(login_required, name='dispatch')
@@ -28,7 +30,7 @@ class SetpackageCreateView(CreateView):
     model = Setpackage
     form_class = SetpackageForm
     template_name = 'setpackages/setpackage_form.html'
-    success_url = reverse_lazy('setpackage_list')  # redirect to list after success
+    success_url = reverse_lazy('setpackage_list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -51,7 +53,6 @@ class SetpackageCreateView(CreateView):
         else:
             self.object = form.save()
 
-            # Save related SetpackageTest entries
             tests = self.request.POST.getlist('tests')
             for test_id in tests:
                 exclusion_field = f'exclusion_{test_id}'
@@ -65,7 +66,7 @@ class SetpackageCreateView(CreateView):
                             exclusion_amount=exclusion_amount
                         )
                     except ValueError:
-                        continue  # skip if the value isn't a valid number
+                        continue
 
             return redirect(self.success_url)
 
@@ -81,22 +82,18 @@ class SetpackageUpdateView(UpdateView):
         context = super().get_context_data(**kwargs)
         all_tests = Test.objects.all()
         existing_tests = {pt.test_id: pt.exclusion_amount for pt in self.object.tests.all()}
-        
-        # Attach exclusion info to test objects for the template
+
         for test in all_tests:
             test.is_checked = test.id in existing_tests
             test.exclusion_amount = existing_tests.get(test.id, "")
-        
+
         context['tests'] = all_tests
         return context
 
     def form_valid(self, form):
         self.object = form.save()
-
-        # Clear existing test associations
         self.object.tests.all().delete()
 
-        # Add updated test associations
         tests = self.request.POST.getlist('tests')
         for test_id in tests:
             exclusion_value = self.request.POST.get(f'exclusion_{test_id}')
@@ -109,10 +106,9 @@ class SetpackageUpdateView(UpdateView):
                         exclusion_amount=exclusion_amount
                     )
                 except ValueError:
-                    continue  # skip invalid input
+                    continue
 
         return redirect(self.success_url)
-
 
 
 @method_decorator(login_required, name='dispatch')
@@ -120,3 +116,19 @@ class SetpackageDeleteView(DeleteView):
     model = Setpackage
     template_name = 'setpackages/setpackage_confirm_delete.html'
     success_url = reverse_lazy('setpackage_list')
+
+
+@login_required
+def get_available_transaction_purposes(request):
+    purposes = list(
+        Setpackage.objects.values_list('package_transaction_purpose', flat=True).distinct()
+    )
+    return JsonResponse({'purposes': purposes})
+
+@require_GET
+@login_required
+def get_tests_by_transaction_purpose(request, purpose):
+    test_ids = SetpackageTest.objects.filter(
+        package__package_transaction_purpose=purpose
+    ).values_list('test_id', flat=True).distinct()
+    return JsonResponse({'test_ids': list(test_ids)})
